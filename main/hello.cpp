b501f1b13f87c1352ff309de18ce2c35880904d5
SINCHANA S <sinchanas1308@gmail.com>
2024-03-05 15:30:36 +0530

    first commit

---
+++ main/hello.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "This is Sinchana trying here";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+
+  return 0;
+}
