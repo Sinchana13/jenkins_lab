6dfbdaa88d21f6e217914c2a25c2abe1953148f1
SINCHANA S <sinchanas1308@gmail.com>
2024-03-06 18:37:51 +0530

    trying webhook

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "This is Sinchana trying here";
+  cout << "This is Sinchana trying here\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "Nice to meet you\n";
 
   return 0;
 }
